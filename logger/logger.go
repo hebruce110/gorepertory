387d021f79f2a3a8b8edce16d2b9d6f588b56a80
Bruce.He <bruce.he@patpat.com>
2019-12-26 18:16:19 +0800

    update

--- logger/logger.go
+++ logger/logger.go
@@ -1,24 +1,37 @@
 package logger
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"os"
+	"path"
+	"runtime"
 	"time"
 )
 
-func Info(info interface{}) {
-	//创建日志文件
+func Info(msg interface{}) {
 	f, err := os.OpenFile("golang_"+time.Now().Format("20060102")+".log", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer f.Close()
+	defer closed(f)
 	writers := []io.Writer{
 		f,
 		os.Stdout,
 	}
 	fileAndStdoutWriter := io.MultiWriter(writers...)
-	logger := log.New(fileAndStdoutWriter, "", log.Ldate|log.Ltime|log.Lshortfile)
-	logger.Println(info)
+	prefix := "[" + time.Now().Format("2006-01-02 15:04:05.000000") + "]" + " " + "Logger.INFO" + " "
+	logger := log.New(fileAndStdoutWriter,prefix,0)
+	//logger := log.New(fileAndStdoutWriter,prefix,log.Ldate|log.Ltime|log.Lmicroseconds)
+	_, file, line, _ := runtime.Caller(1)
+	file_line := fmt.Sprintf("(%v:%v)", path.Base(file), line)
+	logger.Println(file_line,msg)
+}
+
+func closed(f *os.File)  {
+	err := f.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
